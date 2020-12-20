69a657851d25958b8fd3fecf4ec97822abb4c4eb
otrack <0track@gmail.com>
2020-12-20 12:51:39 +0100

    some clean-up; jump to latest DSO client

--- src/main/bin/layer.sh
+++ src/main/bin/layer.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CONFIG_DIR=${DIR}
+LIB_DIR=${DIR}/../../../target/lib
+SRC_DIR=${DIR}/../../../target/
+PROJ_DIR=${DIR}/../../..
+
 source ${DIR}/utils.sh
 
 usage(){
@@ -24,6 +30,7 @@ AWS_S3_KEY=$(config aws.s3.key)
 
 if [[ "$1" == "-create" ]]
 then
+    mvn clean package -DskipTests -f ${PROJ_DIR}
     rm -Rf ${ZIP_DIR}
     mkdir -p ${ZIP_DIR} ${ZIP_DIR}/bin ${ZIP_DIR}/java/lib ${ZIP_DIR}/var/task
     # dependencies
@@ -32,25 +39,26 @@ then
     cp -Rf ${DIR}/tools/* ${ZIP_DIR}/bin
     # build, deploy
     cd ${ZIP_DIR} && zip -qr code.zip * && cd ${DIR}
-    aws s3api put-object --bucket ${AWS_S3_BUCKET} --key ${AWS_S3_KEY} --body ${ZIP_DIR}/code.zip
-    aws --profile=default --region us-east-1 lambda publish-layer-version \
+    aws s3api put-object --bucket ${AWS_S3_BUCKET} --key ${AWS_S3_KEY} --body ${ZIP_DIR}/code.zip > /dev/null
+    version=$(aws --profile=default --region us-east-1 lambda publish-layer-version \
         --layer-name serverless-bash \
         --description serverless-bash \
         --license-info "Apache" \
         --content S3Bucket=${AWS_S3_BUCKET},S3Key=${AWS_S3_KEY} \
-        --compatible-runtimes java11
+        --compatible-runtimes java11 | grep "\"Version\":" | awk -F: '{print $2}' | sed s/[\ ,]//g)
     # FIXME
     aws lambda add-layer-version-permission \
-	--layer-name serverless-bash \
-	--statement-id rights \
-	--version-number 15 \
-	--principal '*' \
-	--action lambda:GetLayerVersion \
-	--output text \
-	--region=${AWS_REGION}
+    	--layer-name serverless-bash \
+    	--statement-id rights \
+    	--version-number ${version} \
+    	--principal '*' \
+    	--action lambda:GetLayerVersion \
+    	--output text \
+    	--region=${AWS_REGION}
 elif [[ "$1" == "-delete" ]]
 then
     echo "NYI"
+    exit 0
 elif [[ "$1" == "-exists" ]]
 then
 	echo "NYI"
