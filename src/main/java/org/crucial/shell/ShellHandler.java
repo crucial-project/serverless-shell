1b8006928362ff62694f96ce22ceb96b5d3d82af
otrack <0track@gmail.com>
2021-01-21 19:06:53 +0100

    indent

--- src/main/java/org/crucial/shell/ShellHandler.java
+++ src/main/java/org/crucial/shell/ShellHandler.java
@@ -6,7 +6,6 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class ShellHandler implements RequestHandler<String, String[]> {
 
