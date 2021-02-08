5d8193c4b80bb800dcd5a63db3d9a165c258084a
Aurèle MAHEO <aurele.maheo@gmail.com>
2021-02-08 12:29:39 +0100

    Add treemap in tests stateful example

--- examples/tests-stateful.sh
+++ examples/tests-stateful.sh
@@ -19,5 +19,6 @@ THREADS=4
 [[ ! -z $(for i in $(seq 1 1 ${THREADS}); do sshell "counter -n test increment -1 1" & done; wait | tail -n 1 | grep $((THREADS+2))) ]] || fail 3
 [[ ! -z $(BARRIER=$(uuid); for i in $(seq 1 1 ${THREADS}); do sshell barrier -n ${BARRIER} -p ${THREADS} await & done | grep ${THREADS}) ]] || fail 4
 [[ ! -z $(MAP=$(uuid); sshell "map -n ${MAP} mergeAll -1 \"1=1\" -1 \"2=1\" -2 sum; for i in \$(map -n ${MAP} keySet); do echo \"\$i=\$(map -n ${MAP} get -1 \$i)\"; done" | grep "1=1") ]] || fail 5
+[[ ! -z $(MAP=$(uuid); sshell "treemap -n ${MAP} mergeAll -1 \"1=1\" -1 \"2=1\" -2 sum; for i in \$(treemap -n ${MAP} keySet); do echo \"\$i=\$(treemap -n ${MAP} get -1 \$i)\"; done" | grep "1=1") ]] || fail 5
 
 success
