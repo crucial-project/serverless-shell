13009b8579703f31d3934dde7beebdd23975714d
EC2 Default User <ec2-user@ip-172-31-57-14.ec2.internal>
2021-03-09 20:21:39 +0000

    Add scp to tools

--- examples/thumbnails.sh
+++ examples/thumbnails.sh
@@ -19,8 +19,15 @@ echo ""
 echo cat refined thumbnails content :
 cat thumbnails_refined_content.out 
 
-cat thumbnails_refined_content.out | parallel -j100 -I,, --env sshell "sshell \" echo do pwd ; pwd ; export HOME=/var/task ; echo HOME ; echo echo \\\$HOME ; echo python path ; type python ; echo LD_LIBRARY_PATH ; echo \\\$LD_LIBRARY_PATH ; find /usr/lib64 -name libfuse* ; find . /lib64 -name libfuse* ; echo MODULES ; ls /lib/modules ; echo MODPROBE ; /usr/bin/depmod ;  modprobe fuse ; cd /tmp ; BASEFILE=${THUMBNAILSBASE}/,, ; echo BASEFILE ; echo \\\$BASEFILE ; FILEINDEX=\\\$(echo \\\$BASEFILE | awk -F'[/.]' '{print \\\$8}') ; echo FILEINDEX : ; echo \\\$FILEINDEX ; wget ${THUMBNAILSBASE}/,, ; cd --  ; echo Check /tmp content : ; ls -alsth /tmp ; magick convert -define png:size=300x100 /tmp/*.png -auto-orient -thumbnail 180x110 -unsharp 0x.5 /tmp/thumbnail\\\$FILEINDEX.png  ; echo CHECK thumbnail result ; ls -alsth /tmp \"" 
+clock1=`date +%s`
+
+cat thumbnails_refined_content.out | parallel -j100 -I,, --env sshell "sshell \" rm -rf /tmp/* ; echo do pwd ; pwd ; export HOME=/var/task ; echo HOME ; echo echo \\\$HOME ; echo LD_LIBRARY_PATH ; echo \\\$LD_LIBRARY_PATH ; cd /tmp ; BASEFILE=${THUMBNAILSBASE}/,, ; echo BASEFILE ; echo \\\$BASEFILE ; FILEINDEX=\\\$(echo \\\$BASEFILE | awk -F'[/.]' '{print \\\$8}') ; echo FILEINDEX : ; echo \\\$FILEINDEX ; wget ${THUMBNAILSBASE}/,, ; cd --  ; echo Check /tmp content : ; ls -alsth /tmp ; magick convert -define png:size=300x100 /tmp/*.png -auto-orient -thumbnail 180x110 -unsharp 0x.5 /tmp/thumbnail\\\$FILEINDEX.png  ; scp /tmp/thumbnail\\\$FILEINDEX.png ${THUMBNAILSBASE} ; echo CHECK thumbnail result ; ls -alsth /tmp \"" 
+
+clock2=`date +%s`
 
 durationthumbnails=`expr $clock2 - $clock1`
 
-echo duration thumbnails: $durationthumbnails seconds
+echo ""
+echo ""
+echo ""
+echo DURATION THUMBNAILS : $durationthumbnails seconds
