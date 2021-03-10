5552e041d75f5c40da7a2ac08526ac995c27583d
EC2 Default User <ec2-user@ip-172-31-51-200.ec2.internal>
2021-03-10 15:45:57 +0000

    update thumbnails.sh

--- examples/thumbnails.sh
+++ examples/thumbnails.sh
@@ -19,9 +19,15 @@ echo ""
 echo cat refined thumbnails content :
 cat thumbnails_refined_content.out 
 
+sshell "rm -rf /tmp/*"
+
+echo ""
+echo ""
+echo START PROCESSING
+
 clock1=`date +%s`
 
-cat thumbnails_refined_content.out | parallel -j100 -I,, --env sshell "sshell \" echo ======= ; echo BEGIN ; echo  ======= ;  rm -rf /tmp/* ; echo do pwd ; pwd ; export HOME=/var/task ; echo HOME ; echo echo \\\$HOME ; echo LD_LIBRARY_PATH ; echo \\\$LD_LIBRARY_PATH ; cd /tmp ; BASEFILE=${THUMBNAILSBASE}/,, ; echo BASEFILE ; echo \\\$BASEFILE ; FILEINDEX=\\\$(echo \\\$BASEFILE | awk -F'[/.]' '{print \\\$8}') ; echo FILEINDEX : ; echo \\\$FILEINDEX ; wget ${THUMBNAILSBASE}/,, ; cd --  ; echo Check /tmp content : ; ls -alsth /tmp ; magick convert -define png:size=300x100 /tmp/*.png -auto-orient -thumbnail 180x110 -unsharp 0x.5 /tmp/thumbnail\\\$FILEINDEX.png  ; scp /tmp/thumbnail\\\$FILEINDEX.png ${THUMBNAILSBASE} ; echo CHECK thumbnail result ; ls -alsth /tmp ; echo ======== ; echo END ; echo ======== \"" 
+cat thumbnails_refined_content.out | parallel -j500 -I,, --env sshell "sshell \" rm -rf /tmp/* ; echo ======= ; echo BEGIN ; echo  ======= ; echo do pwd ; pwd ; export HOME=/var/task ; echo HOME ; echo echo \\\$HOME ; echo LD_LIBRARY_PATH ; echo \\\$LD_LIBRARY_PATH ; cd /tmp ; BASEFILE=${THUMBNAILSBASE}/,, ; echo BASEFILE ; echo \\\$BASEFILE ; FILEINDEX=\\\$(echo \\\$BASEFILE | awk -F'[/.]' '{print \\\$8}') ; echo FILEINDEX : ; echo \\\$FILEINDEX ; wget ${THUMBNAILSBASE}/,, ; cd --  ; echo Check /tmp content : ; ls -alsth /tmp ; echo SLEEP ; sleep 5 ; magick convert -define png:size=300x100 /tmp/*.png -auto-orient -thumbnail 180x110 -unsharp 0x.5 /tmp/THUMBNAIL\\\$FILEINDEX.png ; echo CHECK thumbnail result ; ls -alsth /tmp ; echo SLEEP ; sleep 5 ; echo ======== ; echo END ; echo ======== \"" 
 
 clock2=`date +%s`
 
@@ -31,3 +37,7 @@ echo ""
 echo ""
 echo ""
 echo DURATION THUMBNAILS : $durationthumbnails seconds
+
+
+echo "CHECK AWS LAMBDA /tmp"
+sshell "ls -alsth /tmp"
