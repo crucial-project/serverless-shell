16885bb16a15dcedfc6b3ed9a769669b73ca7c0c
EC2 Default User <ec2-user@ip-172-31-51-200.ec2.internal>
2021-03-10 14:01:44 +0000

    update thumbnails.sh

--- examples/thumbnails.sh
+++ examples/thumbnails.sh
@@ -21,7 +21,7 @@ cat thumbnails_refined_content.out
 
 clock1=`date +%s`
 
-cat thumbnails_refined_content.out | parallel -j100 -I,, --env sshell "sshell \" rm -rf /tmp/* ; echo do pwd ; pwd ; export HOME=/var/task ; echo HOME ; echo echo \\\$HOME ; echo LD_LIBRARY_PATH ; echo \\\$LD_LIBRARY_PATH ; cd /tmp ; BASEFILE=${THUMBNAILSBASE}/,, ; echo BASEFILE ; echo \\\$BASEFILE ; FILEINDEX=\\\$(echo \\\$BASEFILE | awk -F'[/.]' '{print \\\$8}') ; echo FILEINDEX : ; echo \\\$FILEINDEX ; wget ${THUMBNAILSBASE}/,, ; cd --  ; echo Check /tmp content : ; ls -alsth /tmp ; magick convert -define png:size=300x100 /tmp/*.png -auto-orient -thumbnail 180x110 -unsharp 0x.5 /tmp/thumbnail\\\$FILEINDEX.png  ; scp /tmp/thumbnail\\\$FILEINDEX.png ${THUMBNAILSBASE} ; echo CHECK thumbnail result ; ls -alsth /tmp \"" 
+cat thumbnails_refined_content.out | parallel -j100 -I,, --env sshell "sshell \" echo ======= ; echo BEGIN ; echo  ======= ;  rm -rf /tmp/* ; echo do pwd ; pwd ; export HOME=/var/task ; echo HOME ; echo echo \\\$HOME ; echo LD_LIBRARY_PATH ; echo \\\$LD_LIBRARY_PATH ; cd /tmp ; BASEFILE=${THUMBNAILSBASE}/,, ; echo BASEFILE ; echo \\\$BASEFILE ; FILEINDEX=\\\$(echo \\\$BASEFILE | awk -F'[/.]' '{print \\\$8}') ; echo FILEINDEX : ; echo \\\$FILEINDEX ; wget ${THUMBNAILSBASE}/,, ; cd --  ; echo Check /tmp content : ; ls -alsth /tmp ; magick convert -define png:size=300x100 /tmp/*.png -auto-orient -thumbnail 180x110 -unsharp 0x.5 /tmp/thumbnail\\\$FILEINDEX.png  ; scp /tmp/thumbnail\\\$FILEINDEX.png ${THUMBNAILSBASE} ; echo CHECK thumbnail result ; ls -alsth /tmp ; echo ======== ; echo END ; echo ======== \"" 
 
 clock2=`date +%s`
 
