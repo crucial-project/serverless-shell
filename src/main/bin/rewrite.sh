b92c3667e8fa8b2c8b053117db2fb27402e01c5a
Aurèle MAHEO <aurele.maheo@gmail.com>
2021-06-01 19:34:24 +0200

    rewrite.sh: update

--- src/main/bin/rewrite.sh
+++ src/main/bin/rewrite.sh
@@ -19,7 +19,9 @@ patternskip2="mkfifo_pash_fifos()"
 patternskip3="rm -f"
 patternskip4="mkfifo"
 patternskip5="/pash/runtime/eager.sh"
-patternskip6="source"
+patternskip6="/pash/runtime/auto-split.sh"
+patternskip7="source"
+patternskip8="&"
 
 sshell="sshell"
 inputbash="$1"
@@ -31,23 +33,46 @@ do
     #matchpattern=$(echo $line | grep -q "$pattern1" || echo $line | grep -q "$pattern2" || echo $line | grep -q "$pattern3")
     matchpattern4=$(echo $line | grep -q "$pattern4")
     matchpattern3=$(echo $line | grep -q "$pattern3")
-    if echo "$line" | grep -q "$patternskip1" || echo "$line" | grep -q "$patternskip3" || echo "$line" | grep -q "$patternskip4" || echo $line | grep -q "$patternskip5" || echo $line | grep -q "$patternskip6"; then
+    if echo "$line" | grep -q "$patternskip1" || echo "$line" | grep -q "$patternskip3" || echo "$line" | grep -q "$patternskip4" || echo $line | grep -q "$patternskip5" || echo $line | grep -q "$patternskip6" || echo $line | grep -q "$patternskip7"; then
       	#echo HIT
       	continue
     fi
-    if [[ "$line" == *"fifo"* ]] ; then 
-	echo FIFO
-    fi
+    #if [[ "$line" == *"fifo"* ]] ; then 
+    #	echo FIFO
+    #fi
 
+    line=$(echo $line | sed 's/{//g')
+    line=$(echo $line | sed 's/}//g')
+    line=$(echo $line | sed 's/</< /g')
+    line=$(echo $line | sed 's/>/> /g')
+
+    #echo line: $line
+    dumpline=""
+    IFS=', ' read -r -a arrayline <<< "$line"
+    for index in "${!arrayline[@]}"
+    do
+	#echo elem: ${arrayline[index]}
+        if [[ "${arrayline[index]}" == *"tmp"* ]] ; then 
+	        #echo fifo substring: ${arrayline[index]}
+		tmparrayline="/mnt/efsimttsp/uuid"
+		#arrayline[index]=$tmparrayline
+		#tmparrayline=${arrayline[index]}
+        	dumpline="${dumpline} ${tmparrayline}"
+	else
+        	dumpline="${dumpline} ${arrayline[index]}"
+	fi
+        #${tmparrayline//tmp/fs}	
+	#echo tmp: $tmparrayline
+    done
+    #dumpline=$arrayline
+    echo dumpline: $dumpline
+    #echo After line replacement : ${line//tmp*/fs}
     #echo match pattern: $matchpattern
     #if [ -n "$matchpattern4" ]; then
     #  echo HIT
     #  continue
     #fi
     #echo ------------------
-    line=$(echo $line | sed 's/</< /g')
-    line=$(echo $line | sed 's/>/> /g')
-    echo line: $line
     #cmd=$(echo $i | sed 's/"/\\"/g')
     cmd=$(echo $line | sed 's/"/\\"/g')
     #echo CMD: $cmd
